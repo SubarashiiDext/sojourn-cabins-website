524d51581cc2de546e408edef9583a71f0dc0f1b
Prince Kongo <pkkongo@st.ug.edu.gh>
2023-08-25 03:26:45 +0000

    style: The FAQs section on the homepage should be responsive on the mobile/small screens

--- src/components/Home/FaqSection.jsx
+++ src/components/Home/FaqSection.jsx
@@ -2,12 +2,12 @@ import Accordion from "./Accordion";
 
 const FaqSection = () => {
     return (
-        <section className="w-full h-max px-24 flex flex-col items-start overflow-hidden">
+        <section className="w-full h-max px-6 md:px-24 flex flex-col items-start overflow-hidden">
             <h2 className="text-xl font-semibold text-black/80 capitalize">
                 frequently asked questions
             </h2>
-            <div className="w-full flex  justify-between mt-6">
-                <div className="w-1/2 space-y-4 mr-2">
+            <div className="w-full flex flex-col md:flex-row justify-between mt-6">
+                <div className="w-full md:w-1/2 space-y-4 md:mr-2 pb-4 md:pb-0">
                     <Accordion title="What's included in our getaway?">
                         <p className="font-medium text-dark/80">
                             Please see up to date pricing for our cabin and all add-on
@@ -20,27 +20,27 @@ const FaqSection = () => {
                             services via our website: https://www.sojourngh.com/rates
                         </p>
                     </Accordion>
-                    <Accordion title="What time are check in and check out?">
+                    <Accordion title="Do you have 24/7 security?">
                         <p className="font-medium text-dark/80">
                             Please see up to date pricing for our cabin and all add-on
                             services via our website: https://www.sojourngh.com/rates
                         </p>
                     </Accordion>
-                    <Accordion title="Does Sojourn Cabins have a restaurant?">
+                    <Accordion title="Do you offer restaurants?">
                         <p className="font-medium text-dark/80">
                             Please see up to date pricing for our cabin and all add-on
                             services via our website: https://www.sojourngh.com/rates
                         </p>
                     </Accordion>
                 </div>
-                <div className="w-1/2 space-y-4 ml-2">
+                <div className="w-full md:w-1/2 space-y-4 md:ml-2">
                     <Accordion title="Where is the cabin located?">
                         <p className="font-medium text-dark/80">
                             Please see up to date pricing for our cabin and all add-on
                             services via our website: https://www.sojourngh.com/rates
                         </p>
                     </Accordion>
-                    <Accordion title="Are customers able to visit the cabin without a booking?">
+                    <Accordion title="Do I need to sign up to book?">
                         <p className="font-medium text-dark/80">
                             Please see up to date pricing for our cabin and all add-on
                             services via our website: https://www.sojourngh.com/rates
