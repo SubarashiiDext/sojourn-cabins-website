54d35fe4636c807877a817a974af56418e7e997a
Phinehas Borketey <phinehasborketey42@gmail.com>
2023-08-25 15:36:23 +0000

    feat: each cabin card on the search results page can be selected

--- src/components/search-results-card/page.jsx
+++ src/components/search-results-card/page.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import Image from "next/image";
-import {MdOutlineLocationOn} from "react-icons/md";
-import { cabinData } from '../Modules/card-dummy-data/page';
-
+import {MdOutlineLocationOn, MdRadioButtonUnchecked} from "react-icons/md";
+import { useState } from 'react';
+import {BsCheckCircleFill} from "react-icons/bs"
 
 export default function ResultsCard({id, location, price, people, name, image, thumbnail}) {
+  const [select, setSelect] = useState(false);
+
+  function handleClick(){ 
+    setSelect  (!select)
+  }
+ 
+
+
   return (
+    
+
     <div>
 
-    
+
+
       <div className='flex items-center justify-center  '>
-        <div className='mb-2 px-2 py-2 border-2 border-gray-200  rounded shadow-md hover:border-gray-100 hover:shadow-lg transition duration-200'>
-          <div className='rounded  overflow-hidden h-40  bg-slate-400'>
-            <Image className='object-cover object-center rounded h-40' src={thumbnail} alt="" width={282} height={336}/>
+        <div className={`mb-2 px-2 py-2 border-2 ${select ? "border-blue-500 hover:border-blue-300 bg-blue-100 ": "border-gray-200"} rounded shadow-md hover:border-gray-100 hover:shadow-lg transition duration-200`}>
+          <div className='relative rounded  overflow-hidden h-40  '>
+            <Image className='object-cover object-center rounded h-40 ' src={thumbnail} alt="" width={282} height={336}/>
+            {select ? 
+              (<BsCheckCircleFill onClick={handleClick} className='absolute top-2 right-2 bg-white rounded-[50%] bg-opacity-70 text-blue-500 '  size={30}/>)
+              : 
+              (<MdRadioButtonUnchecked onClick={handleClick} className='absolute top-2 right-2 bg-black rounded-[50%] bg-opacity-50 text-white '  size={30}/>) 
+            }
+            {/* <MdRadioButtonUnchecked className='absolute top-2 right-2 bg-black rounded-[50%] bg-opacity-50 text-white '  size={30}/> */}
+            {/* <input type='radio' className='absolute top-2 right-2 text-gray-400 ' size={30}/> */}
+
           </div>
           
           <div className='my-2'>
